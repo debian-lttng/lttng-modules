32f9acb75f6de85526c0c8a027ef0eaf61a4d87f
Michael Jeanson <mjeanson@debian.org>
2020-02-26 11:12:30 -0500

    New upstream version 2.11.2

--- wrapper/time.h
+++ wrapper/time.h
@@ -2,29 +2,21 @@
  *
  * wrapper/time.h
  *
- * Copyright (C) 2015 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
+ * Copyright (C) 2020 Michael Jeanson <mjeanson@efficios.com>
+ * Copyright (C) 2020 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
  */
 
 #ifndef _LTTNG_WRAPPER_TIME_H
 #define _LTTNG_WRAPPER_TIME_H
 
 #include <linux/version.h>
-#include <linux/time.h>
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,4,0))
-
-static inline bool lttng_close_on_exec(int fd, const struct fdtable *fdt)
-{
-	return close_on_exec(fd, fdt);
-}
-
-#else
-
-static inline bool lttng_close_on_exec(int fd, const struct fdtable *fdt)
-{
-	return FD_ISSET(fd, fdt->close_on_exec);
-}
-
+/*
+ * Use 64bit timespec on kernels that have it, this makes 32bit arch
+ * y2038 compliant.
+ */
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,17,0))
+# define LTTNG_KERNEL_HAS_TIMESPEC64
 #endif
 
 #endif /* _LTTNG_WRAPPER_TIME_H */
